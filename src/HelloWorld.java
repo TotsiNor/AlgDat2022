9a7c31653b5d9b86fdae6064edfb5fed56d1370b
Sondre Trodahl <Sondretotsdal@gmail.com>
2022-08-23 14:08:50 +0200

    ok

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Teller til ti:");
+        for(int i = 1; i<11; i++)
+        {
+            System.out.println(i);
+            System.out.println("Yo!");
+        }
+    }
+}
+
